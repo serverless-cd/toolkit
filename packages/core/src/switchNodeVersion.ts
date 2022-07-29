0a3d15b7b4ed4eff7210446f7d9f6b20bd3d3ee4
wss-git <wssgryx@163.com>
2022-07-29 15:11:38 +0800

    feat: support nvm 12/16

---
+++ packages/core/src/switchNodeVersion.ts
@@ -0,0 +1,21 @@
+import { get, isEqual, has } from 'lodash';
+
+const DEFAULT_PATH = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin';
+const VERSIONS = {
+  '12': 'v12.22.12',
+  '16': 'v16.16.0',
+};
+
+export default function (version: string) {
+  if (isEqual(version, '14')) {
+    return;
+  }
+  if (!has(VERSIONS, version)) {
+    throw new Error(`Version ${version} is not supported`);
+  }
+
+  const versionTag = get(VERSIONS, 'version');
+  process.env.PATH = `/home/node/.nvm/versions/node/${versionTag}/bin:${process.env.PATH || DEFAULT_PATH}`;
+  process.env.NVM_INC = `/home/node/.nvm/versions/node/${versionTag}/include/node`;
+  process.env.NVM_BIN = `/home/node/.nvm/versions/node/${versionTag}/bin`;
+}
