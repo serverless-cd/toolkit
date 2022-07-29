07d1373fd198032e621bc7862d201d9332424814
wss-git <wssgryx@163.com>
2022-07-29 16:22:44 +0800

    feat: support logger

--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -1,7 +1,36 @@
 import fse from 'fs-extra';
 
-export default function logger(message: string, filePath: string) {
-  console.log(message);
+export default class logger {
+  static enableDebug() {
+    process.env.enable_logger_debug = 'true';
+  }
 
-  fse.appendFileSync(filePath, message);
+  static closeDebug() {
+    process.env.enable_logger_debug = 'false';
+  }
+
+  static isDebug() {
+    return process.env.enable_logger_debug === 'true';
+  }
+  
+  static debug(message: string, filePath: string) {
+    if (this.isDebug()) {
+      this.appendFile(filePath, message);
+      console.log(message);
+    }
+  }
+
+  static info(message: string, filePath: string) {
+    this.appendFile(filePath, message);
+    console.log(message);
+  }
+
+  static error(message: string, filePath: string) {
+    this.appendFile(filePath, message);
+    console.log(message);
+  }
+
+  private static appendFile(message: string, filePath: string) {
+    fse.appendFileSync(filePath, message);
+  }
 }
