3796ef2bb88e57c9cb0aec820e6c94e9dc7f4e74
xsahxl <xsahxl@126.com>
2022-08-24 09:42:06 +0800

    core export lodash ...

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,6 @@ export { default as logger } from './logger';
 export { default as parseSpec } from './parse-spec';
 export * from './switch-node-version';
 export * from './variable';
+export { default as fse } from 'fs-extra';
+export { default as lodash } from 'lodash';
+export { default as jsyaml } from 'js-yaml';
