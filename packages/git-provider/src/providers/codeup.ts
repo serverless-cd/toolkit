42e5c743d7aede02aa36efe76e51aae431a1d860
huangzichun <313658356@qq.com>
2023-01-04 20:05:35 +0800

    feat: 🎸 codeup create fork

--- packages/git-provider/src/providers/codeup.ts
+++ packages/git-provider/src/providers/codeup.ts
@@ -172,6 +172,9 @@ export default class Codeup {
   putFile(params: any): Promise<void> {
     throw new Error('Method not implemented.');
   }
+  createFork(params: any): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
 
   private _test_debug_log(data: any, log: string = 'test') {
     try {
