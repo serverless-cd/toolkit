fcd3ddaceb2edd1b3a4d8014427d827481d59e0a
huangzichun <313658356@qq.com>
2023-02-16 15:23:49 +0800

    fix: 🐛 delete aliyun_client

--- packages/aliyun-client/__tests__/aliyun-client.test.ts
+++
@@ -1,9 +0,0 @@
-import Client from '../src';
-
-const config = {
-  accessKeyId: process.env.ACCESS_KEY_ID || '',
-  accessKeySecret: process.env.ACCESS_KEY_SECRET || '',
-};
-
-const client = new Client(config);
-client.getFc2({ region: 'cn-hangzhou' });
